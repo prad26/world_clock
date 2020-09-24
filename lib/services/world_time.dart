33c407c6a76fbf849a0ce4d9e51ac39ed0f99dc1
prad26 <pradyumna.banakar@gmail.com>
2020-09-24 23:09:31 +0530

    Custom WorldTime Class

---
+++ lib/services/world_time.dart
@@ -0,0 +1,29 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+  String location;
+  String time;
+  String flag;
+  String url;
+
+  WorldTime({this.location, this.flag, this.url});
+
+  Future<void> getTime() async {
+    // make the request
+    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
+    Map data = jsonDecode(response.body);
+    // get properties from json
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    //print(datetime);
+    //print(offset);
+
+    // create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    // set time property
+    time = now.toString();
+  }
+}

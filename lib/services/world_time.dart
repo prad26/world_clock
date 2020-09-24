722e34a5deffa621c5e96b70eeb093bc5bc27c9d
prad26 <pradyumna.banakar@gmail.com>
2020-09-25 01:02:55 +0530

    Decoration Image

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -7,6 +7,7 @@ class WorldTime {
   String time;
   String flag;
   String url;
+  bool isDaytime;
 
   WorldTime({this.location, this.flag, this.url});
 
@@ -28,6 +29,7 @@ class WorldTime {
       now = now.add(Duration(hours: int.parse(hourOffset), minutes: int.parse(minuteOffset)));
 
       // set time property
+      isDaytime = now.hour > 6 && now.hour < 20 ? true : false;
       time = DateFormat.jm().format(now);
     } catch (error) {
       print('caught error: $error');
